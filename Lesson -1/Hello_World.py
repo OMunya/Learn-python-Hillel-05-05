b6823ddbc12cef979efed8211c2d58687f2db561
Oleksandr <aleksandrmnko@gmail.com>
2023-05-11 19:18:29 +0300

    Home Task

---
+++ Lesson -1/Hello_World.py
@@ -0,0 +1,19 @@
+# Литералы - это все значения, не запианные в переменные: 5, 0, -10, "Hello world"
+
+
+print(50)
+print()
+print("Hello world")
+
+# name = input("What is your name?")
+# print("Hello,", name, end="!\n")
+
+name = input("Whrite your name, please: ")
+print(f"Hello, {name}!")  # f- строки
+
+a = 5
+b = 8
+c = b == a + 3
+print(c)  # True
+
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Learn-python-Hillel-05-05
 Learn material for Python Basic course with Kyrylo
+
+
+we are learning pythom with Kyrylo
+
+Commit  - 
+Clone - 
+Repository - Место хранения кода
+Push - 
+Pull - 
